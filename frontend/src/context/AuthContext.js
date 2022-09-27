16d99ef2ede2bb40262c61edd0a60880d2d5c3fc
magda <m.goorna@gmail.com>
2022-09-27 09:58:12 +0200

    auth type change

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -22,7 +22,7 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(()=>{
     const user = JSON.parse(localStorage.getItem('user'))
     if(user){
-      dispatch({type:useLogin, payload: user})
+      dispatch({type:'LOGIN', payload: user})
     }
   },[])
   
